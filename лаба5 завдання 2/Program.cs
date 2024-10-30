7f138a2c4390c35cd2358d36c7d076c3f80debf1
Даша Шубіна <shubinad268@gmail.com>
2024-10-30 18:59:17 +0200

    Add project files.

---
+++ лаба5 завдання 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба5_завдання_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-10, 10);
+                Console.WriteLine($"array[{i}] = {array[i]}");
+
+            }
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > 0; j--)
+                {
+                    if ((array[j] < 0) && (array[j] < array[j - 1]))
+                    {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("--------------------------");
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"array[{i}] = {array[i]} ");
+        }
+    }
+}
